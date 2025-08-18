b1a42cefd690c5d2a304879ecc22c049d11e4e4b
MaskINB <168406807+MaskINB@users.noreply.github.com>
2025-08-19 00:08:40 +0530

    new test cases added

---
+++ arrays/reverseArray.js
@@ -0,0 +1,26 @@
+// reverseArray.js
+
+/**
+ * Reverses an array in place
+ * @param {Array} arr - The array to reverse
+ * @returns {Array} - The reversed array
+ */
+function reverseArray(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    // Swap elements
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+
+  return arr;
+}
+
+// Export function for Jest testing
+module.exports = reverseArray;
+
+// Optional: Simple console test
+// console.log(reverseArray([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
