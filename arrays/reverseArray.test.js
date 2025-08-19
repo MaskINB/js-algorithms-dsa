416e0f390243a42a505e87669ee71a162c73afe8
MaskINB <168406807+MaskINB@users.noreply.github.com>
2025-08-19 13:38:42 +0530

    change the testing library

--- arrays/reverseArray.test.js
+++ arrays/reverseArray.test.js
@@ -1,18 +1,7 @@
-// reverseArray.test.js
-const reverseArray = require('./reverseArray');
+import { describe , test ,it ,expect } from 'vitest';
+import reverseArray from './reverseArray.js';
 
-test('reverses [1, 2, 3, 4, 5] to [5, 4, 3, 2, 1]', () => {
+it('reverses [1, 2, 3, 4, 5] to [5, 4, 3, 2, 1]', () => {
   expect(reverseArray([1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1]);
 });
 
-test('reverses empty array to empty array', () => {
-  expect(reverseArray([])).toEqual([]);
-});
-
-test('reverses [10, 20, 30] to [30, 20, 10]', () => {
-  expect(reverseArray([10, 20, 30])).toEqual([30, 20, 10]);
-});
-
-test('reverses [1] to [1]', () => {
-  expect(reverseArray([1])).toEqual([1]);
-});
