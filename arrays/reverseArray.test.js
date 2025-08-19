c59c77ec767c2a65caa3be30dab764e1014d26e1
MaskINB <168406807+MaskINB@users.noreply.github.com>
2025-08-19 17:45:44 +0530

    new test case added

--- arrays/reverseArray.test.js
+++ arrays/reverseArray.test.js
@@ -5,3 +5,7 @@ it('reverses [1, 2, 3, 4, 5] to [5, 4, 3, 2, 1]', () => {
   expect(reverseArray([1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1]);
 });
 
+
+it ('reverse [10,20,30,40] to [40,30,20,10]',()=> {
+  expect (reverseArray([10,20,30,40])).toEqual([40,30,20,10]);
+})
