fb4311d86ae3fd0dcbac55e67e43124b3465fc3c
MaskINB <168406807+MaskINB@users.noreply.github.com>
2025-09-07 22:23:02 +0530

    fix the code error

--- trees/maxDepth.js
+++ trees/maxDepth.js
@@ -1,10 +1,45 @@
-function maxDepth(node){
-    if(node == null){
-        return 0;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+export default class BinarySearchTree {
+    constructor() {
+        this.root = null;
     }
 
-    const leftDepth = maxDepth(node.left);
-    const rightDepth = maxDepth(node.right);
+    insert(value) {
+        this.root = this.insertBST(this.root, value);
+    }
 
-    return 1 + Math.max(leftDepth, rightDepth);
+    insertBST(node, value) {
+        if (node === null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = this.insertBST(node.left, value);
+        } else {
+            node.right = this.insertBST(node.right, value);
+        }
+        return node;
+    }
+
+    maxDepth() {
+        return this.calculateMaxDepth(this.root);
+    }
+
+    calculateMaxDepth(node) {
+        if (node == null) {
+            return 0;
+        }
+
+        let leftDepth = this.calculateMaxDepth(node.left);
+        let rightDepth = this.calculateMaxDepth(node.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
 }
